9df67ae318ad4ae84a3b9a92527ad19796328a0b
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2013-04-19 16:41:38 +0200

    Add crawler.py, can parse meta file into map

---
+++ metadata-crawler/crawler.py
@@ -0,0 +1,24 @@
+import ConfigParser
+import sys
+
+def parse_datafile(file_name):
+  data = {}
+  for line in open(file_name):
+    line = line.strip()
+    key, value = tuple(entry.strip() for entry in line.split(':', 1))
+    data[key] = value
+  return data
+
+def main():
+  # import configuration
+  config = ConfigParser.SafeConfigParser()
+  config.read('../config.cfg')
+
+  api_key = config.get('Flickr API Key', 'key')
+  metadata_dir = '../' + config.get('Directories', 'metadata-dir')
+
+  # open metadata file
+  print parse_datafile(metadata_dir + '/10-19/10/100000.txt')
+
+if __name__ == '__main__':
+    main()
