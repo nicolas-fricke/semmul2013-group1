e9036fad6ad8aad95a9cfedee32c23398cd06575
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2013-04-25 18:13:40 +0200

    Fix to metafile traversing

--- metadata-crawler/crawler.py
+++ metadata-crawler/crawler.py
@@ -68,7 +68,7 @@ def query_flickr(flickrAPI, photo_id):
 
 def find_metafiles_to_process(metadata_path):
   txt_files  = glob.glob(metadata_path + '/*/*/*.txt')
-  json_files = glob.glob(metadata_path + '/metadata/*/*/*.json')
+  json_files = glob.glob(metadata_path + '/*/*/*.json')
   return [txt_file for txt_file in txt_files if (txt_file[:-3] + 'json') not in json_files]
 
 def main():
