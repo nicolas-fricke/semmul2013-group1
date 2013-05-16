19244c0784067b512fe7481719d61c57a96447e7
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2013-05-16 14:50:05 +0200

    Add tag_co_occurrences_histogram

--- semantic-clustering/tag_preprocessing.py
+++ semantic-clustering/tag_preprocessing.py
@@ -22,7 +22,7 @@ from pprint import pprint
 def read_tags_from_json(json_data):
   tag_list = []
   for tag in json_data["metadata"]["info"]["tags"]["tag"]:
-    tag_list.append(tag["raw"])
+    tag_list.append(tag["_content"])
   return tag_list
 
 def get_json_files(metadata_dir):
@@ -46,13 +46,16 @@ def main():
   # read json files from metadata directory
   json_files = get_json_files(metadata_dir)
 
-  # get list of all tags and count there co-occurences
+  # get list of all tags and count there co-occurrences
   tag_histogram = Counter()
+  tag_co_occurrence_histogram = Counter()
   for json_file in json_files:
     tag_list = read_tags_from_file(json_file)
     if not tag_list == None:
       tag_histogram.update(tag_list)
-  print tag_histogram
+      tag_co_occurrence_histogram.update([(tag1,tag2) for tag1 in tag_list for tag2 in tag_list if tag1 < tag2])
+  #print tag_histogram
+  print tag_co_occurrence_histogram
 
 if __name__ == '__main__':
     main()
