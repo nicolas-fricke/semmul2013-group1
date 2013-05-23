feafdf0dcacbbe5ae4588f620139ffd86c266dbb
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2013-05-23 15:06:14 +0200

    Add color clustering attempt, using HSV space

---
+++ feature-extractor/color-clustering.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Script crawls images from flickr and parses them in SimpleCV
+#
+#
+# Specify the metadata folder in the config file: ../config.cfg
+#
+# Use config.cfg.template as template for this file
+#
+#
+# author: nicolas fricke
+# mail: nicolas.fricke@student.hpi.uni-potsdam.de
+################################################################################
+
+import ConfigParser
+import json
+from collections import defaultdict
+from SimpleCV import Image
+from Pycluster import kcluster
+from helpers import *
+
+def main():
+  # import configuration
+  config = ConfigParser.SafeConfigParser()
+  config.read('../config.cfg')
+
+  api_key = config.get('Flickr API Key', 'key')
+  metadata_dir = '../' + config.get('Directories', 'metadata-dir')
+
+  metajson_files = find_metajsons_to_process(metadata_dir)
+
+  print_status("Reading metadata files, loading images and calculating color histograms.... ")
+  images = []
+  for file_number, metajson_file in enumerate(metajson_files):
+    metadata = parse_json_file(metajson_file)
+    if metadata["stat"] == "ok":
+      data = {}
+      url      = get_small_image_url(metadata)
+      data["image_id"]  = metadata["id"]
+      data["file_path"] = metajson_file
+      data["url"]       = url
+      try:
+        image = Image(url).toHSV()
+      except Exception:
+        continue
+      bins = split_image_into_bins(image, 25)
+      data["colors"] = []
+      for bin in bins:
+        (hue, saturation, value) = bin.splitChannels()
+        data["colors"] += hue.histogram(20)
+        data["colors"] += saturation.histogram(20)
+        data["colors"] += value.histogram(20)
+      (hue, saturation, value) = image.splitChannels()
+      images.append(data)
+    if file_number > 200:
+      break
+  print "Done."
+
+  print_status("Building data structure for clustering.... ")
+  colors = []
+  for image_data in images:
+    colors.append(image_data["colors"])
+  print "Done."
+
+  print_status("Clustering images by color histograms via k-means algorithm.... ")
+  clustered_images, value, _ = kcluster(colors, 20)
+  print "Done."
+
+  clusters = defaultdict(list)
+  for index, cluster in enumerate(clustered_images):
+    clusters[cluster].append(images[index])
+
+  write_clusters_to_html(clusters)
+
+if __name__ == '__main__':
+    main()
---
+++ feature-extractor/helpers.py
@@ -0,0 +1,64 @@
+from math import sqrt
+from SimpleCV import Image
+from glob import glob
+import json
+import sys
+import time
+
+def print_status(message):
+  sys.stdout.write(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()) + " - ")
+  sys.stdout.write(message)
+  sys.stdout.flush()
+
+def find_metajsons_to_process(metadata_path):
+  return glob(metadata_path + '/*/*/*.json')
+
+def parse_json_file(json_file):
+  with open(json_file) as file:
+    return json.load(file)
+
+def get_small_image_url(metajson):
+  if not metajson.get("stat") == "ok":
+    return None
+  sizes = metajson["metadata"]["sizes"]
+  if not sizes.get("stat") == "ok":
+    return None
+  return [entry["source"] for entry in sizes["sizes"]["size"] if entry["label"] == "Small"][0]
+
+def write_clusters_to_html(clusters, html_file="out.html"):
+  output_html =  ( "<html>\n"
+                   "  <head>\n"
+                   "    <title>Cluster Visualization</title>\n"
+                   "    <style>\n"
+                   "      table {width: 100%; table-layout: fixed; border-collapse: collapse;}\n"
+                   "      tr > td:first-child {text-align: center; font-weight: bold;}"
+                   "      td {border: 2px solid black; margin: 5px;}\n"
+                   "      .images {white-space: nowrap; overflow: scroll;}\n"
+                   "    </style>\n"
+                   "  </head>\n"
+                   "  <body>\n"
+                   "    <table border='1'>\n"
+                   "      <tr><th style='width: 100px'>Cluster</th><th>Images</th></tr>\n")
+  for cluster_number, images in clusters.iteritems():
+    output_html += "      <tr><td>#%d<br/><i>(%d images)</i></td><td class='images'>\n" % (cluster_number, len(images))
+    for image in images:
+      output_html += "        <img src='%s' />\n" % image["url"]
+    output_html += "      </td></tr>\n"
+  output_html += ( "    </table>\n"
+                   "  </body>\n"
+                   "</html>")
+  with open(html_file, "w") as output_file:
+    output_file.write(output_html)
+
+def split_image_into_bins(image, bins):
+  bins_sqrt = int(sqrt(bins))
+  if bins != bins_sqrt ** 2:
+    raise Exception("Number of bins must be a square root")
+  step_width  = image.width  / bins_sqrt
+  step_height = image.height / bins_sqrt
+  image_slices = []
+  # Last few pixels get lost, if width mod bins_sqrt != 0
+  for pos_height in range(0, step_height * bins_sqrt, step_height):
+    for pos_width in range(0, step_width * bins_sqrt, step_width):
+      image_slices.append(image.crop(pos_width, pos_height, step_width, step_height))
+  return tuple(image_slices)
