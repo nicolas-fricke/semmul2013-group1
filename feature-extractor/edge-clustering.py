a25e94b94adace8d153648d17f3ed9f9e15c737c
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2013-05-23 16:30:15 +0200

    Use helper.py in edge-clustering

--- feature-extractor/edge-clustering.py
+++ feature-extractor/edge-clustering.py
@@ -15,53 +15,12 @@
 ################################################################################
 
 import ConfigParser
-import sys
-import time
 import json
-import glob
 from collections import defaultdict
 from SimpleCV import Image
 from SimpleCV import EdgeHistogramFeatureExtractor
 from Pycluster import kcluster
-
-def print_status(message):
-  sys.stdout.write(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()) + " - ")
-  sys.stdout.write(message)
-  sys.stdout.flush()
-
-def find_metajsons_to_process(metadata_path):
-  return glob.glob(metadata_path + '/*/*/*.json')
-
-def parse_json_file(json_file):
-  with open(json_file) as file:
-    return json.load(file)
-
-def get_small_image_url(metajson):
-  if not metajson.get("stat") == "ok":
-    return None
-  sizes = metajson["metadata"]["sizes"]
-  if not sizes.get("stat") == "ok":
-    return None
-  return [entry["source"] for entry in sizes["sizes"]["size"] if entry["label"] == "Small"][0]
-
-def write_clusters_to_html(clusters, html_file="out.html"):
-  output_html =  ( "<html>"
-                   "  <head>"
-                   "    <title>Cluster Visualization</title>"
-                   "  </head>"
-                   "  <body>"
-                   "    <table border='1'>"
-                   "      <tr><th>Cluster</th><th>Images</th></tr>")
-  for cluster_number, images in clusters.iteritems():
-    output_html += "      <tr><td>%d</td><td>" % cluster_number
-    for image in images:
-      output_html += "        <img src='%s' />" % image["url"]
-    output_html += "      </td></tr>"
-  output_html += ( "    </table>"
-                   "  </body>"
-                   "</html>")
-  with open(html_file, "w") as output_file:
-    output_file.write(output_html)
+from helpers import *
 
 def main():
   # import configuration
@@ -74,7 +33,7 @@ def main():
   metajson_files = find_metajsons_to_process(metadata_dir)
 
   print_status("Reading metadata files, loading images and calculating edge histograms.... ")
-  edgeExtractor = EdgeHistogramFeatureExtractor()
+  edgeExtractor = EdgeHistogramFeatureExtractor(bins=4)
   images = []
   for file_number, metajson_file in enumerate(metajson_files):
     metadata = parse_json_file(metajson_file)
@@ -88,16 +47,18 @@ def main():
         image = Image(url)
       except Exception:
         continue
-      data["edges"]     = edgeExtractor.extract(image)
+      edge_angles_and_lengths = edgeExtractor.extract(image)
+      data["edge-angles"]      = edge_angles_and_lengths[:len(edge_angles_and_lengths) / 2]
+      data["edge-lengths"]     = edge_angles_and_lengths[len(edge_angles_and_lengths) / 2:]
       images.append(data)
-    if file_number > 100:
+    if file_number > 200:
       break
   print "Done."
 
   print_status("Building data structure for clustering.... ")
   edges = []
   for image_data in images:
-    edges.append(image_data["edges"])
+    edges.append(image_data["edge-angles"] + image_data["edge-lengths"])
   print "Done."
 
   print_status("Clustering images by edge histograms via k-means algorithm.... ")
