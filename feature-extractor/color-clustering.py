54e93c4edca3d37dd410b3e3320cf1674927d084
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2013-05-30 12:25:46 +0200

    Fix order of HSV -> VSH after splitChannels

--- feature-extractor/color-clustering.py
+++ feature-extractor/color-clustering.py
@@ -48,7 +48,7 @@ def main():
       bins = split_image_into_bins(image, 25)
       data["colors"] = []
       for bin in bins:
-        (hue, saturation, value) = bin.splitChannels()
+        (value, saturation, hue) = bin.splitChannels()
         data["colors"] += hue.histogram(20)
         data["colors"] += saturation.histogram(20)
         data["colors"] += value.histogram(20)
