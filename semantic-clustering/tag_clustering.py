db6c945cf30316ae2ea3aa0a6f5a3a1437da5729
Mandy Roick <mandy.roick@student.hpi.uni-potsdam.de>
2013-06-03 20:33:36 +0200

    refactor tag_clustering, make tag_co_occurrence_histogram global

--- semantic-clustering/tag_clustering.py
+++ semantic-clustering/tag_clustering.py
@@ -16,14 +16,18 @@ import operator
 from collections import defaultdict
 from collections import Counter
 
+tag_co_occurrence_histogram = Counter()
+tag_index_dict = dict()
 
 ################     Laplace Matrix       ###################################
 
-def calculate_negative_adjacency_matrix(tag_index_dict, tag_co_occurrence_histogram):
-  negative_adjacency_matrix = np.zeros((len(tag_index_dict),len(tag_index_dict)))
-  for (tag1, tag2) in tag_co_occurrence_histogram:
-    negative_adjacency_matrix[tag_index_dict[tag1]][tag_index_dict[tag2]] = -1
-    negative_adjacency_matrix[tag_index_dict[tag2]][tag_index_dict[tag1]] = -1
+def calculate_negative_adjacency_matrix(tag_list):
+  negative_adjacency_matrix = np.zeros((len(tag_list),len(tag_list)))
+  for index1 in range(0, len(tag_list)):
+    for index2 in range(index1, len(tag_list)):
+      if ((tag_list[index1], tag_list[index2]) in tag_co_occurrence_histogram) or ((tag_list[index2], tag_list[index1]) in tag_co_occurrence_histogram):
+        negative_adjacency_matrix[index1][index2] = -1
+        negative_adjacency_matrix[index2][index1] = -1
   return negative_adjacency_matrix
 
 # set diagonal of laplace to degrees of vertices
@@ -32,8 +36,8 @@ def add_diagonal_matrix_to_adjacency(matrix):
     matrix[i][i] = sum(line)*(-1)
   return matrix
 
-def calculate_laplace_matrix(tag_index_dict, tag_co_occurrence_histogram):
-  laplace_matrix = calculate_negative_adjacency_matrix(tag_index_dict, tag_co_occurrence_histogram)
+def calculate_laplace_matrix(tag_list):
+  laplace_matrix = calculate_negative_adjacency_matrix(tag_list)
   print "Done calculation of adjacency matrix"
   laplace_matrix = add_diagonal_matrix_to_adjacency(laplace_matrix)
   print "Done calculation of diagonal matrix"
@@ -70,33 +74,33 @@ def calculate_second_smallest_eigen_vector(eigen_values, eigen_vectors):
         index_of_second_smallest_eigen_value = index
   return eigen_vectors[index_of_second_smallest_eigen_value]
 
-def create_clusters(separation_vector, index_tag_dict):
+def create_clusters(partitioning_vector, tag_list):
   tag_cluster1 = []
   tag_cluster2 = []
 
   # '>=' instead of '>' results in overlapping clusters
-  for index, vector_value in enumerate(separation_vector):
+  for index, vector_value in enumerate(partitioning_vector):
     if vector_value > 0:
-      tag_cluster1.append(index_tag_dict[index])
+      tag_cluster1.append(tag_list[index])
     if vector_value < 0:
-      tag_cluster2.append(index_tag_dict[index])
+      tag_cluster2.append(tag_list[index])
 
   return tag_cluster1, tag_cluster2
 
-def spectral_bisection(matrix, index_tag_dict):
+def spectral_bisection(matrix, tag_list):
   eigen_values, eigen_vectors = linalg.eig(matrix)
   print "Done calculate eigenvalues"
   partitioning_vector = calculate_second_smallest_eigen_vector(eigen_values, eigen_vectors)
   print "Done find vector for second highest eigenvalue"
-  return create_clusters(partitioning_vector, index_tag_dict)
+  return create_clusters(partitioning_vector, tag_list)
 
 
 ################     Calculate Q       ###################################
 
-def calculate_parent_weight(tag_co_occurrence_histogram):
+def calculate_parent_weight():
   return 2*sum(tag_co_occurrence_histogram.values())
 
-def calculate_child_weights(tag_co_occurrence_histogram, cluster1, cluster2):
+def calculate_child_weights(cluster1, cluster2):
   child1_weight = 0
   child2_weight = 0
   for (tag1, tag2), weight in tag_co_occurrence_histogram.items():
@@ -110,7 +114,7 @@ def calculate_child_weights(tag_co_occurrence_histogram, cluster1, cluster2):
   child2_weight *= 2
   return child1_weight, child2_weight
 
-def calculate_inter_cluster_weights(tag_co_occurrence_histogram, cluster1, cluster2):
+def calculate_inter_cluster_weights(cluster1, cluster2):
   inter_cluster_child1_parent_weight = 0
   inter_cluster_child2_parent_weight = 0
   for (tag1, tag2), weight in tag_co_occurrence_histogram.items():
@@ -126,17 +130,17 @@ def calculate_modularity_of_child_cluster(child_weight, inter_cluster_weight, pa
   return ((child_weight/float(parent_weight)) - ((inter_cluster_weight/float(parent_weight))*(inter_cluster_weight/float(parent_weight))))
 
 # calculate modularity function Q
-def calculate_Q(tag_co_occurrence_histogram, cluster1, cluster2):
+def calculate_Q(cluster1, cluster2):
   # A(V,V)
-  parent_weight = calculate_parent_weight(tag_co_occurrence_histogram)
+  parent_weight = calculate_parent_weight()
   print "Done calculate parent_weight"
 
   # A(Vc,Vc)
-  child1_weight, child2_weight = calculate_child_weights(tag_co_occurrence_histogram, cluster1, cluster2)
+  child1_weight, child2_weight = calculate_child_weights(cluster1, cluster2)
   print "Done calculate child weights"
 
   # A(Vc,V)
-  inter_cluster_child1_parent_weight, inter_cluster_child2_parent_weight = calculate_inter_cluster_weights(tag_co_occurrence_histogram, cluster1, cluster2)
+  inter_cluster_child1_parent_weight, inter_cluster_child2_parent_weight = calculate_inter_cluster_weights(cluster1, cluster2)
   inter_cluster_child1_parent_weight += child1_weight
   inter_cluster_child2_parent_weight += child2_weight
   print "Done calculate inter cluster weights"
@@ -149,14 +153,13 @@ def calculate_Q(tag_co_occurrence_histogram, cluster1, cluster2):
 
 ################     Recursive Partitioning       ###################################
 
-def partitioning(tag_index_dict, tag_co_occurrence_histogram):
-  # create laplace matrice
-  laplace_matrix = calculate_laplace_matrix(tag_index_dict, tag_co_occurrence_histogram)
+def partitioning(tag_list):
+  # create laplace matrix
+  laplace_matrix = calculate_laplace_matrix(tag_list)
   print "Done creating laplace matrix"
 
-  # create two overlapping clusters
-  index_tag_dict = dict(zip(tag_index_dict.values(), tag_index_dict.keys()))
-  cluster1, cluster2 = spectral_bisection(laplace_matrix, index_tag_dict)
+  # create two not overlapping clusters
+  cluster1, cluster2 = spectral_bisection(laplace_matrix, tag_list)
   print "Done group into 2 child clusters"
   #pprint.pprint(cluster1)
   #pprint.pprint(cluster2)
@@ -187,35 +190,39 @@ def split_co_occurence_histogram(co_occurrence_histogram, cluster1, cluster2):
       co_occurrence_histogram2[(tag1, tag2)] = value
   return co_occurrence_histogram1, co_occurrence_histogram2
 
-def recursive_partitioning(tag_index_dict, tag_co_occurrence_histogram):
+def recursive_partitioning(tag_list):
   tag_clusters = []
-  cluster1, cluster2 = partitioning(tag_index_dict, tag_co_occurrence_histogram)
+  cluster1, cluster2 = partitioning(tag_list)
    # calculate modularity function Q whether partition is useful
-  q = calculate_Q(tag_co_occurrence_histogram, cluster1, cluster2)
+  q = calculate_Q(cluster1, cluster2)
   print "Done calculate q: %f" % q
   print "######################     Done with one bisection        ###################################"
   if q > 0:
-    tag_index_dict1, tag_index_dict2 = split_dictionary(tag_index_dict, cluster1, cluster2)
-    #print tag_index_dict1
-    #print tag_index_dict2
-    tag_co_occurrence_histogram1, tag_co_occurrence_histogram2 = split_co_occurence_histogram(tag_co_occurrence_histogram, cluster1, cluster2)
-    #print tag_co_occurrence_histogram1
-    #print tag_co_occurrence_histogram2
     if len(cluster1) > 1:
-      tag_clusters.extend(recursive_partitioning(tag_index_dict1, tag_co_occurrence_histogram1))
+      tag_clusters.extend(recursive_partitioning(cluster1))
+    else:
+      tag_clusters.append(cluster1)
     if len(cluster2) > 1:
-      tag_clusters.extend(recursive_partitioning(tag_index_dict2, tag_co_occurrence_histogram2))
+      tag_clusters.extend(recursive_partitioning(cluster2))
+    else:
+      tag_cluster.append(cluster2)
   else:
     print "------------------------- Q < 0 ------------------------------"
-    return [tag_index_dict.keys()]
+    return [tag_list]
   return tag_clusters
 
 
 ################     Tag Clustering       ###################################
 
-def tag_clustering(tag_index_dict, tag_co_occurrence_histogram):
-  tag_clusters= recursive_partitioning(tag_index_dict, tag_co_occurrence_histogram)
+def tag_clustering(index_dict, co_occurrence_histogram):
+  global tag_co_occurrence_histogram
+  global tag_index_dict
+  tag_index_dict = index_dict
+  tag_co_occurrence_histogram = co_occurrence_histogram
+  tag_clusters = recursive_partitioning(index_dict.keys())
+
   for tag_cluster in tag_clusters:
     print tag_cluster
   print "%d clusters" % len(tag_clusters)
+
   return tag_clusters
