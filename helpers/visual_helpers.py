1bf2e245266af1adabea51f15de4700a0b61d46f
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2013-06-13 16:27:55 +0200

    Add helpler for recursively slicing image

--- helpers/visual_helpers.py
+++ helpers/visual_helpers.py
@@ -13,3 +13,15 @@ def split_image_into_bins(image, bins):
     for pos_width in range(0, step_width * bins_sqrt, step_width):
       image_slices.append(image.crop(pos_width, pos_height, step_width, step_height))
   return tuple(image_slices)
+
+def split_image_into_slices(image, slice_number):
+  if slice_number == 0:
+    return []
+  step_height = image.height / slice_number
+  step_width  = image.width  / slice_number
+  slice_list  = []
+  for i in range(0, slice_number):
+    slice_list.append(image.crop(i * step_width, 0, step_width, image.height))
+    slice_list.append(image.crop(0, i * step_height, image.width, step_height))
+  slice_list += split_image_into_slices(image, slice_number - 1)
+  return slice_list
