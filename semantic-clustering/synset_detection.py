44553c7c4427cf34ecbb490e98bb4d76438b61a1
Claudia Exeler <cl.ex@gmx.de>
2013-06-13 16:50:41 +0200

    refactore and write pickle file of preprocessed data

--- semantic-clustering/synset_detection.py
+++ semantic-clustering/synset_detection.py
@@ -1,8 +1,7 @@
 import ConfigParser
 import json
 from nltk.corpus import wordnet as wn
-import re # Regex
-import string
+from collections import defaultdict
 import operator
 
 # Import own modules
@@ -13,37 +12,38 @@ from tag_preprocessing import *
 
 
 def parse_json_data(json_files, number_of_jsons):
-  synsets = {}
-  for count,json_file in enumerate(json_files):
-    if count > number_of_jsons:
+  synsets_for_pictures = defaultdict(list)
+  for index_of_json, json_file in enumerate(json_files):
+    if index_of_json > number_of_jsons:
       break
-    synsets[count] = []
     tag_list, photo_data = read_data_from_json_file(json_file)
-    synset_lists = []
     if tag_list == None:
       continue
     print "############ ", photo_data["url"], " ##########"
+
+    synsets_for_pictures[photo_data["url"]] = []
+    indefinite_synsets = []
+
     for tag in tag_list:
       synsets_for_tag = wn.synsets(tag, pos=wn.NOUN)
       if len(synsets_for_tag) == 1:
-        synsets[count].append(synsets_for_tag[0])
+        synsets_for_pictures[photo_data["url"]].append(synsets_for_tag[0])
         print "Eindeutiges Synset: " + str(synsets_for_tag[0])
       elif len(synsets_for_tag) > 1:
-        synset_lists.append(synsets_for_tag)
+        indefinite_synsets.append(synsets_for_tag)
 
-    for synset_list in synset_lists:
-      #initialize index to 0 so that if no tags with single synset (=synsetes[count] empty), first synset will be chosen
+    for indefinite_synset in indefinite_synsets:
+      #initialize index to 0 so that if no tags with single synset (=synsetes[index_of_json] empty), first synset will be chosen
       max_sim_sum = (0, 0)
-      for index, synset in enumerate(synset_list):
+      for index, synset in enumerate(indefinite_synset):
         sim_sum = 0
-        for found_synset in synsets[count]:
+        for found_synset in synsets_for_pictures[photo_data["url"]]:
           sim_sum += synset.lch_similarity(found_synset)
         if sim_sum > max_sim_sum[0]:
           max_sim_sum = (sim_sum, index)
-      synsets[count].append(synset_list[max_sim_sum[1]])
-      print "Ermitteltes Synset: ", (synset_list[max_sim_sum[1]]), " (", synset_list[max_sim_sum[1]].definition, ")"
-
-      
+      synsets_for_pictures[photo_data["url"]].append(indefinite_synset[max_sim_sum[1]])
+      print "Ermitteltes Synset: ", (indefinite_synset[max_sim_sum[1]]), " (", indefinite_synset[max_sim_sum[1]].definition, ")"
+  return synsets_for_pictures
 
 def synset_detection(number_of_jsons):
   # import configuration
@@ -56,8 +56,12 @@ def synset_detection(number_of_jsons):
 
   # parse data from json files
   print_status("Parsing json files, creating histograms and tags dictionary... ")
-  parse_json_data(json_files,number_of_jsons)
+  keywords_for_pictures = parse_json_data(json_files,number_of_jsons)
   print "Done, with %2d Tags" % len(tag_histogram)
+
+  print_status("Write preprocessed data to file ...")
+  save_object(keywords_for_pictures, "preprocessed_data")
+  print "Done"
 
   # tag index dict saves the matching index of a tag in the laplace matrix
   print_status("Creating tag list... ")
