e8f30dd60921c6e2d7bb716a15ea616c8d729c00
Claudia Exeler <cl.ex@gmx.de>
2013-06-20 16:28:52 +0200

    adapt pipeline to new tree structure

--- semantic-clustering/pipeline.py
+++ semantic-clustering/pipeline.py
@@ -5,19 +5,19 @@ from general_helpers import load_object
 
 
 def recursively_find_pictures_for_synset_tree(nodes, synsets_to_filenames_dict, find_pictures_for_hyponyms=False, find_pictures_for_meronyms=False):
-  for node_name, node_values in nodes.iteritems():
-    if find_pictures_for_hyponyms and node_values["hyponyms"] != None:
-      node_values["hyponyms"] = recursively_find_pictures_for_synset_tree(node_values["hyponyms"], synsets_to_filenames_dict, find_pictures_for_hyponyms=True, find_pictures_for_meronyms=find_pictures_for_meronyms)
-    if find_pictures_for_meronyms and node_values["meronyms"] != None:
-      node_values["meronyms"] = recursively_find_pictures_for_synset_tree(node_values["meronyms"], synsets_to_filenames_dict, find_pictures_for_hyponyms=find_pictures_for_hyponyms, find_pictures_for_meronyms=True)
-    node_values["picture_filenames"] = synsets_to_filenames_dict[node_name]
-    #if len(node_values["picture_filenames"]) > 0:
-    #  print node_values["picture_filenames"]
+  for node in nodes:
+    if find_pictures_for_hyponyms and node.has_hyponyms():
+      print node.hyponyms
+      recursively_find_pictures_for_synset_tree(node.hyponyms, synsets_to_filenames_dict, find_pictures_for_hyponyms=True, find_pictures_for_meronyms=find_pictures_for_meronyms)
+    if find_pictures_for_meronyms and node.has_meronyms():
+      recursively_find_pictures_for_synset_tree(node.meronyms, synsets_to_filenames_dict, find_pictures_for_hyponyms=find_pictures_for_hyponyms, find_pictures_for_meronyms=True)
+    node.associated_pictures = synsets_to_filenames_dict[node.name]
   return nodes
 
-# def find_pictures_for_synset_tree(hyponyms_trees):
-#   for key, value in hyponyms_trees:
-#     recursively_find_pictures_for_synset_tree(value)
+# def find_pictures_for_synset_tree(hyponyms_trees, synsets_to_filenames_dict, find_pictures_for_hyponyms=True, find_pictures_for_meronyms=True):
+#   for node in hyponyms_trees:
+#     recursively_find_pictures_for_synset_tree(node, synsets_to_filenames_dict, find_pictures_for_hyponyms=find_pictures_for_hyponyms, find_pictures_for_meronyms=find_pictures_for_meronyms)
+#   return hyponyms_trees
     
 
 def main(argv):
@@ -30,15 +30,16 @@ def main(argv):
   hyponyms_trees = find_hyponyms_on_wordnet(word)
   print "Done."
 
-  #pretty_print_dict(hyponyms_trees)
+  #pretty_print_tree(hyponyms_trees)
 
-  print_status("Done. Found %d entries.\n" % count_nested_dict(hyponyms_trees))
+  print_status("Done. Found %d entries.\n" % count_tree_nodes(hyponyms_trees))
 
   synsets_to_filenames_dict = load_object("synset_filenames_dict_100.pickle")
 
   hyponyms_trees_with_filenames = recursively_find_pictures_for_synset_tree(hyponyms_trees, synsets_to_filenames_dict, find_pictures_for_hyponyms=True, find_pictures_for_meronyms=True)
 
-  pretty_print_dict(hyponyms_trees_with_filenames)
+  for synset in hyponyms_trees_with_filenames:
+    pretty_print_tree(synset)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
