ffb23e0161754ba7a0e0dd0b8b90bb48f236ed73
Claudia Exeler <cl.ex@gmx.de>
2013-06-20 17:08:36 +0200

    first advances with templates in flask

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template('result.html', greeting='Claudia')
 
 if __name__ == "__main__":
   app.run()
