7221a78377d49e9f526ef1efd224cf6919014828
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2013-07-03 22:54:25 +0200

    Rename read_tags_from_json -> read_keywords_from_json
    Remove unused code in tag_preprocessing.py

--- src/clustering/semantic/tag_preprocessing.py
+++ src/clustering/semantic/tag_preprocessing.py
@@ -43,7 +43,7 @@ def hypernym_is_color(tag):
   return False
 
 # probably add preprocessing steps for tags
-def read_tags_from_json(json_data):
+def read_keywords_from_json(json_data):
   tag_list = []
   for raw_tag in json_data["metadata"]["info"]["tags"]["tag"]:
     # Preprocess tag before appending to tag_list
@@ -70,7 +70,7 @@ def read_data_from_json_file(json_file):
   if json_data["stat"] == "ok":
     data["image_id"]  = json_data["id"]
     data["url"] = get_small_image_url(json_data) # TODO: could return None, does this have to be catched somehow?
-    return read_tags_from_json(json_data), data
+    return read_keywords_from_json(json_data), data
   return None, None
 
 def import_metadata_dir_of_config(path):
@@ -186,25 +186,6 @@ def tag_preprocessing(number_of_jsons):
   print_status("Parsing json files, creating histograms and tags dictionary... ")
   tag_histogram, tag_co_occurrence_histogram, tag_similarity_histogram, photo_tags_dict, photo_data_list = parse_json_data(json_files,number_of_jsons)
   print "Done, with %2d Tags" % len(tag_histogram)
-
-  #write_tag_similarity_histogram_to_file(tag_similarity_histogram)
-
-  # remove X percent tags with too small occurence from the histograms
-  #cut_off_percentage = 10
-  #print_status("Removing the lower %d%% of tags from histograms... " % cut_off_percentage)
-  #tag_histogram, tag_co_occurrence_histogram, tag_similarity_histogram = remove_tags_with_small_occurence(cut_off_percentage)
-  #print "Done, %2d Tags remaining" % len(tag_histogram)
-
-  # remove tags with too small and too high Tf-idf value (occure too seldom or too often)
-  # sorted_tag_histogram = sorted(tag_histogram.iteritems(), key=operator.itemgetter(1))
-  # print "Tag \t Frequency \t Tf-idf"
-  # (_,highest_val) = sorted_tag_histogram[-1]
-  # for (key, val) in sorted_tag_histogram:
-  #   tf_idf = 0
-  #   for photo_taglist in photo_tags_dict.values():
-  #     if key in photo_taglist:
-  #       tf_idf += log(number_of_jsons/float(val)) # * freq(val) / freq(highest_val)
-  #   print "%s \t %d \t %f" % (key,val,tf_idf)
 
   # tag index dict saves the matching index of a tag in the laplace matrix
   print_status("Creating tag list... ")
