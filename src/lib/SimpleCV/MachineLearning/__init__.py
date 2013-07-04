da502768e7f556864345573450a16683b21c4631
Mandy Roick <mandy.roick@student.hpi.uni-potsdam.de>
2013-07-04 17:58:27 +0200

    Delete lib because does not work

--- src/lib/SimpleCV/MachineLearning/__init__.py
+++
@@ -1,4 +0,0 @@
-from SimpleCV.MachineLearning.SVMClassifier import *
-from SimpleCV.MachineLearning.TreeClassifier import *
-from SimpleCV.MachineLearning.KNNClassifier import *
-from SimpleCV.MachineLearning.NaiveBayesClassifier import *
