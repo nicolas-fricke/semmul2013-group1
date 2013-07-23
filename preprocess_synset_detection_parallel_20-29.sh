39a3d437873a187a2cdfc7d79b6b58658bc1d508
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2013-07-23 21:06:23 +0200

    Add script for preprocess synset detection in parallel

---
+++ preprocess_synset_detection_parallel_20-29.sh
@@ -0,0 +1,11 @@
+mkdir -p log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/20 &) > log/preprocess_synset_detection_20.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/21 &) > log/preprocess_synset_detection_21.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/22 &) > log/preprocess_synset_detection_22.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/23 &) > log/preprocess_synset_detection_23.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/24 &) > log/preprocess_synset_detection_24.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/25 &) > log/preprocess_synset_detection_25.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/26 &) > log/preprocess_synset_detection_26.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/27 &) > log/preprocess_synset_detection_27.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/28 &) > log/preprocess_synset_detection_28.log
+(cd src && nohup python clustering/semantic/synset_detection_bestfirstsearch.py -d /20-29/29 &) > log/preprocess_synset_detection_29.log
