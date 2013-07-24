9a6825f35f6535433e217e7af1cbb137fa32a2e6
Claudia Exeler <cl.ex@gmx.de>
2013-07-24 11:31:00 +0200

    read jsons instead of pickles

--- src/clustering/pipeline.py
+++ src/clustering/pipeline.py
@@ -60,7 +60,7 @@ def get_searchtrees_with_filenames(search_term, use_meronyms, minimal_node_size)
   synset_tag_tf_idf_dict_filename = config.get('Filenames for Pickles', 'synset-tag-cooccurrence-dict')
 
   ####### WordNet Search #######
-  tf_idf_tuple = load_object(synset_tag_tf_idf_dict_filename)
+  tf_idf_tuple = parse_json_file(synset_tag_tf_idf_dict_filename)
 
   print_status("Running WordNet Search for %s... " % search_term)
   hyponyms_trees = construct_searchtree(search_term, tf_idf_tuple, use_meronyms)
@@ -69,8 +69,8 @@ def get_searchtrees_with_filenames(search_term, use_meronyms, minimal_node_size)
   #pretty_print_tree(hyponyms_trees)
   #print_status("Found %d entries.\n" % count_tree_nodes(hyponyms_trees))
 
-  synsets_to_filenames_dict = load_object(synset_filenames_dict_filename)
-  tags_to_filenames_dict = load_object(tag_filenames_dict_filename)
+  synsets_to_filenames_dict = parse_json_file(synset_filenames_dict_filename)
+  tags_to_filenames_dict = parse_json_file(tag_filenames_dict_filename)
 
   searchtrees_with_pictures, _ = recursively_find_pictures_for_synset_tree(hyponyms_trees, synsets_to_filenames_dict,
                                               find_pictures_for_hyponyms=True, find_pictures_for_meronyms=True,
